8687b613334706762565a8021a683a2d26c4eaf8
bilmoh287 <142092794+bilmoh287@users.noreply.github.com>
2025-10-05 00:13:28 +0300

    Added GlobalUser in PL for storing logged in user info

---
+++ LibraryPresentationLayer/GlobalUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryBussinessLayer;
+
+namespace LibraryPresentationLayer
+{
+    public class clsGlobalUser
+    {
+        public static clsUsers CurrentUser { get; set; }
+    }
+}
