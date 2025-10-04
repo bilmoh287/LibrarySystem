aa9688e3fae33b5355a300364d8ada50a2bc08c5
bilmoh287 <142092794+bilmoh287@users.noreply.github.com>
2025-10-05 01:15:57 +0300

    Role based permission for Managing all Forms

--- LibraryPresentationLayer/frmMainMenu.cs
+++ LibraryPresentationLayer/frmMainMenu.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using LibraryBussinessLayer;
 
 namespace LibraryPresentationLayer
 {
@@ -19,24 +20,52 @@ namespace LibraryPresentationLayer
 
         private void btnManageBooks_Click(object sender, EventArgs e)
         {
+            if (!clsGlobalUser.CurrentUser.HasPermission(clsUsers.Permissions.ManageAuthors))
+            {
+                MessageBox.Show("You don’t have permission to Manage Authors.", "Access Denied",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
             frmBooks frm = new frmBooks();
             frm.ShowDialog();
         }
 
         private void btnManageAuthors_Click(object sender, EventArgs e)
         {
+            if (!clsGlobalUser.CurrentUser.HasPermission(clsUsers.Permissions.ManageAuthors))
+            {
+                MessageBox.Show("You don’t have permission to Manage Authors.", "Access Denied",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
             frmAuthors frm = new frmAuthors();
             frm.ShowDialog();
         }
 
         private void btnManageUsers_Click(object sender, EventArgs e)
         {
+            if (!clsGlobalUser.CurrentUser.HasPermission(clsUsers.Permissions.ManageUsers))
+            {
+                MessageBox.Show("You don’t have permission to Manage Users.", "Access Denied",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
             frmUsers frm = new frmUsers();
             frm.ShowDialog();
         }
 
         private void btnBorrowReturn_Click(object sender, EventArgs e)
         {
+            if (!clsGlobalUser.CurrentUser.HasPermission(clsUsers.Permissions.BorrowReturn))
+            {
+                MessageBox.Show("You don’t have permission to Borrow Books.", "Access Denied",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
             frmBorrowReturnBooks frm = new frmBorrowReturnBooks();
             frm.ShowDialog();
         }
